07eeb3e943debf59cea7bd20a14015fb31905ee8
Minh Tiến Dương <okay7290@gmail.com>
2023-07-06 03:43:23 +0700

    update

--- components/home/StatisChainTable/index.js
+++ components/home/StatisChainTable/index.js
@@ -69,67 +69,74 @@ export default function StatisChainTable() {
         setDataTable(dt);
     }
 
-    return <Container className={styles['container']}><table className={styles['table']}>
-        <thead>
-            <tr>
-                <th className={styles['chain']}>CHAIN</th>
-                <th className={styles['commits']}>
-                    <div className={styles['sort-table']} onClick={() => sort('commits')}>
-                        <label>COMMITS</label>
-                        <IconSort direct={directSort?.commits} />
-                    </div>
-                </th>
-                <th>
-                    <div className={styles['sort-table']} onClick={() => sort('contributors')}>
-                        <label>CONTRIBUTORS</label>
-                        <IconSort direct={directSort?.contributors} />
-                    </div>
-                </th>
-                <th>
-                    <div className={styles['sort-table']} onClick={() => sort('issues_solved')}>
-                        <label>ISSUES SOLVED</label>
-                        <IconSort direct={directSort?.issues_solved} />
-                    </div>
-                </th>
-                <th>
-                    <div className={styles['sort-table']} onClick={() => sort('stars')}>
-                        <label>STARS</label>
-                        <IconSort direct={directSort?.stars} />
-                    </div>
-                </th>
-                <th>
-                    <div className={styles['sort-table']} onClick={() => sort('forks')}>
-                        <label>FORKS</label>
-                        <IconSort direct={directSort?.forks} />
-                    </div>
-                </th>
-            </tr>
-        </thead>
+    console.log('a');
 
-        <tbody>
-            {
-
-                dataTable.map((item, index) => {
-                    return <tr key={item?.id} className={styles['row']} onClick={() => {
-                        router.push(`/projects/${item?.github_prefix}`)
-                    }}>
-                        <td>
-                            <div className={styles['chain']}>
-                                <img className={styles['logo']} src={item?.avatar} />
-                                <div className={styles['infor-chain']}>
-                                    <label className={styles['name']}>{item?.name}</label>
-                                    <label className={styles['des']}>{item?.github_prefix}</label>
-                                </div>
+    return <Container className={styles['container']}>
+        <div className={styles['container-inside']}>
+            <table className={styles['table']}>
+                <thead>
+                    <tr>
+                        <th className={styles['chain']}>CHAIN</th>
+                        <th className={styles['commits']}>
+                            <div className={styles['sort-table']} onClick={() => sort('commits')}>
+                                <label>COMMITS</label>
+                                <IconSort direct={directSort?.commits} />
                             </div>
-                        </td>
-                        <td className={styles['commits']}>{item?.total_commit}</td>
-                        <td className={styles['contributors']}>{item?.total_contributor}</td>
-                        <td className={styles['issues-solved']}>{item?.total_issue_solved}</td>
-                        <td className={styles['stars']}>{item?.total_star}</td>
-                        <td className={styles['forks']}>{item?.total_fork}</td>
+                        </th>
+                        <th>
+                            <div className={styles['sort-table']} onClick={() => sort('contributors')}>
+                                <label>CONTRIBUTORS</label>
+                                <IconSort direct={directSort?.contributors} />
+                            </div>
+                        </th>
+                        <th>
+                            <div className={styles['sort-table']} onClick={() => sort('issues_solved')}>
+                                <label>ISSUES SOLVED</label>
+                                <IconSort direct={directSort?.issues_solved} />
+                            </div>
+                        </th>
+                        <th>
+                            <div className={styles['sort-table']} onClick={() => sort('stars')}>
+                                <label>STARS</label>
+                                <IconSort direct={directSort?.stars} />
+                            </div>
+                        </th>
+                        <th>
+                            <div className={styles['sort-table']} onClick={() => sort('forks')}>
+                                <label>FORKS</label>
+                                <IconSort direct={directSort?.forks} />
+                            </div>
+                        </th>
                     </tr>
-                })
-            }
-        </tbody>
-    </table></Container>
+                </thead>
+
+                <tbody>
+                    {
+
+                        dataTable.map((item, index) => {
+                            return <tr key={item?.id} className={styles['row']} onClick={() => {
+                                router.push(`/projects/${item?.github_prefix}`)
+                            }}>
+                                <td>
+                                    <div className={styles['chain']}>
+                                        <img className={styles['logo']} src={item?.avatar} />
+                                        <div className={styles['infor-chain']}>
+                                            <label className={styles['name']}>{item?.name}</label>
+                                            <label className={styles['des']}>{item?.github_prefix}</label>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className={styles['commits']}>{item?.total_commit}</td>
+                                <td className={styles['contributors']}>{item?.total_contributor}</td>
+                                <td className={styles['issues-solved']}>{item?.total_issue_solved}</td>
+                                <td className={styles['stars']}>{item?.total_star}</td>
+                                <td className={styles['forks']}>{item?.total_fork}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </Container>
+
 }

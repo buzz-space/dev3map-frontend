2148358ec3cea21437d37c5cc3d4587a0125909c
Minh Tiến Dương <okay7290@gmail.com>
2023-07-13 21:27:29 +0700

    update logo

--- components/common/ActiveDevelopers/ChartActiveAll/index.js
+++ components/common/ActiveDevelopers/ChartActiveAll/index.js
@@ -1,19 +1,15 @@
 import { useEffect, useState } from "react"
 import ChartLayout from "../ChartLayout"
 import { handleMonth } from "~/utils/strings";
-import moment from "moment";
 
 export default function ChartActiveAll({ data }) {
     function process() {
         return {
-            labels: data?.map((dt) => moment(dt?.day).format('YYYY')),
+            labels: data?.map((dt) => dt?.year),
             datasets: [{
                 label: 'TOTAL MONTHLY ACTIVE DEVS',
                 data: data?.map((dt) => Number(dt?.total_full_time) + Number(dt?.total_part_time)),
-                labelT: data?.map((dt) => {
-                    // `${handleMonth(dt?.month)}/${dt?.year}`
-                    return moment(dt?.day).format('DD-MM-YYYY');
-                }),
+                labelT: data?.map((dt) => `${handleMonth(dt?.month)}/${dt?.year}`),
                 backgroundColor: '#BB86FC',
                 borderColor: '#BB86FC',
             }]
--- components/common/ActiveDevelopers/ChartActiveByType/index.js
+++ components/common/ActiveDevelopers/ChartActiveByType/index.js
@@ -1,38 +1,28 @@
 import { handleMonth } from "~/utils/strings";
 import ChartLayout from "../ChartLayout";
 import { useEffect, useState } from "react";
-import moment from "moment";
 
 
 export default function ChartActiveByType({ data }) {
     function process() {
         return {
-            labels: data?.map((dt) => moment(dt?.day).format('YYYY')),
+            labels: data?.map((dt) => dt?.year),
             datasets: [{
                 label: 'FULL-TIME DEVS',
                 data: data?.map((dt) => dt?.total_full_time),
-                labelT: data?.map((dt) => {
-                    // `${handleMonth(dt?.month)}/${dt?.year}`
-                    return moment(dt?.day).format('DD-MM-YYYY');
-                }),
+                labelT: data?.map((dt) => `${handleMonth(dt?.month)}/${dt?.year}`),
                 backgroundColor: '#BB86FC',
                 borderColor: '#BB86FC',
             }, {
                 label: 'PART-TIME DEVS',
                 data: data?.map((dt) => dt?.total_part_time),
-                labelT: data?.map((dt) => {
-                    // `${handleMonth(dt?.month)}/${dt?.year}`
-                    return moment(dt?.day).format('DD-MM-YYYY');
-                }),
+                labelT: data?.map((dt) => `${handleMonth(dt?.month)}/${dt?.year}`),
                 backgroundColor: '#03DAC6',
                 borderColor: '#03DAC6',
             }, {
                 label: 'ONE-TIME DEVS',
                 data: data?.map((dt) => dt?.total_one_time),
-                labelT: data?.map((dt) => {
-                    // `${handleMonth(dt?.month)}/${dt?.year}`
-                    return moment(dt?.day).format('DD-MM-YYYY');
-                }),
+                labelT: data?.map((dt) => `${handleMonth(dt?.month)}/${dt?.year}`),
                 backgroundColor: '#18A0FB',
                 borderColor: '#18A0FB',
             }]

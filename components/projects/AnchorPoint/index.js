57a56ed6ef020a416cd3c17a911ec09f2c58deae
Quang Nguyen <quangkl198@gmail.com>
2023-10-13 16:50:14 +0700

    update feedback

--- components/projects/AnchorPoint/index.js
+++ components/projects/AnchorPoint/index.js
@@ -1,47 +1,22 @@
-import React from 'react'
-import styles from "./styles.module.scss";
+import React from 'react';
+import styles from './styles.module.scss';
 import Link from 'next/link';
 
-const AnchorPoint = () => {
-    return (
-        <div className={styles['anchor-point']}>
-            <label className={styles['network']}>Aura Network</label>
-            <div className={styles['anchor-nav']}>
-                {
-                    [
-                        {
-                            label: 'Resources',
-                            to: '#resources'
-                        },
-                        // {
-                        //     label: 'Airdrops',
-                        //     to: '#airdrops',
-                        // },
-                        {
-                            label: 'GitHub Statistics',
-                            to: '#github-statistics'
-                        },
-                        {
-                            label: 'Activity Trend',
-                            to: '#activity-trend'
-                        },
-                        {
-                            label: 'Repositories',
-                            to: '#repositories'
-                        },
-                        {
-                            label: 'Contributors',
-                            to: '#contributors'
-                        }
-                    ]?.map((item, index) => {
-                        return <Link className={styles['anchor-link']} href={item?.to} key={index}>
-                            {item?.label}
-                        </Link>
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+const AnchorPoint = ({ data, title }) => {
+  return (
+    <div className={styles['anchor-point']}>
+      <label className={styles['network']}>{title}</label>
+      <div className={styles['anchor-nav']}>
+        {data?.map((item, index) => {
+          return (
+            <Link className={styles['anchor-link']} href={item?.to} key={index}>
+              {item?.label}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default AnchorPoint
+export default AnchorPoint;

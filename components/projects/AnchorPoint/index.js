506ad5eb2543889b792e05b54ef772b740ead2c2
okay7290 <okay7290@gmail.com>
2023-10-20 16:44:13 +0700

    update

--- components/projects/AnchorPoint/index.js
+++ components/projects/AnchorPoint/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './styles.module.scss';
 import Link from 'next/link';
+import { useRef } from 'react';
 
 const AnchorPoint = ({ data, title }) => {
+
+  const ref = useRef();
+
+  useEffect(() => {
+    function scroll() {
+      const el = ref.current;
+      if (window.scrollY > 50) {
+        el.style.visibility = "visible";
+        el.style.opacity = "1";
+        el.style.position = "fixed";
+      } else {
+        el.style.visibility = "hidden";
+        el.style.opacity = "0";
+        el.style.position = "absolute";
+      }
+    }
+    window.addEventListener('scroll', scroll);
+    scroll();
+    return () => {
+      window.removeEventListener('scroll', scroll)
+    }
+  }, [])
+
   return (
-    <div className={styles['anchor-point']}>
+    <div className={styles['anchor-point']} ref={ref}>
       <label className={styles['network']}>{title}</label>
       <div className={styles['anchor-nav']}>
         {data?.map((item, index) => {
